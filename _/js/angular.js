987954fb1b38eba5422b8d7a9d1ad4924969feab
Ruby You <shih@svisualjoint.co.uk>
2014-07-22 02:53:58 +0100

    Clean out angular and add phone gap

--- _/js/angular.js
+++ _/js/angular.js
@@ -3,50 +3,46 @@ var app = angular.module("app",[]).config(function($routeProvider){
 	$routeProvider.when('/d3Charts',{
 		templateUrl:'d3Charts.html',
 		//controller:'d3ChartsController'
-	});
+	})
 
-	$routeProvider.when('/d3Cluster',{
+	.when('/d3Cluster',{
 		templateUrl:'d3Cluster.html',
 		//controller:'d3ClusterController'
-	});
+	})
 
-	$routeProvider.when('/angularTemplate',{
-		templateUrl:'angularTemplate.html',
-		controller:'angularTemplateController'
-	});
+	.when('/angularCMS',{
+		templateUrl:'angularCMS.html',
+		controller:'angularCMSController'
+	})
+	.when('/angularCMS/:itemId',{
+		templateUrl:'angularDetail.html',
+		controller:'angularDetailController'
+	})
 
-	$routeProvider.when('/css3animate',{
-		templateUrl:'css3animate.html',
-		controller:'css3animateController'
-	});
+	.when('/phonegap',{
+		templateUrl:'phonegap.html',
+		controller:'phonegapController'
+	})
 
-	$routeProvider.when('/phpAjax',{
+	.when('/phpAjax',{
 		templateUrl:'phpAjax.html',
 		controller:'phpAjaxController'
-	});
+	})
 
-	$routeProvider.otherwise({
-		redirectTo:'/login'
+	.otherwise({
+		redirectTo:'/angularCMS'
 	});
 });
 
-/*app.controller('LoginController',function($scope,$location){
-	$scope.credentials = {username:"",password:""};
-
-	$scope.login =function(){
-		if($scope.credentials.username == "lulu"){
-			$location.path('home');
-		}
-	}
-});*/
-
 app.factory('myService', function($http){
 	return{
+		// not use now
 		getTeam: function() {
 		return $http.get('_/json/team.json').then(function (result) {
 		return result.data;
 		})
 		},
+		// not use now
 		getNotes: function() {
 		return $http.get('_/json/teamNotes.json').then(function (result) {
 		return result.data;
@@ -62,18 +58,26 @@ app.factory('myService', function($http){
 
 
 
-app.controller('angularTemplateController',function($scope, myService){
+app.controller('angularCMSController',function($scope, myService){
+	$scope.title = "Angular Data binding";
+	$scope.intro = "This is a small example to display startup's resources.";
+	$scope.layout = 'grid';
 	$scope.resources = myService.getResource();
-	$scope.notes = myService.getNotes();
 });
 
-app.controller('css3animateController',function($scope){
-	$scope.title = "Css3 animation - Taking stuff from 2013 early stage websites";
-	$scope.intro = "Coming Soon";
+app.controller('angularDetailController', function($scope, myService, $routeParams) {
+	$scope.title = "Angular Data binding";
+	$scope.intro = "This is a small example to display startup's resources.";
+    $scope.people = myService.getResource();
+    $scope.whichItem = $routeParams.itemId;
+});
+
+app.controller('phonegapController',function($scope){
+	$scope.title = "Phonegap + Jquery presentation";
+	$scope.intro = "Two small projects done in the 2011 winter";
 });
 
 app.controller('phpAjaxController',function($scope){
-	$scope.title = "PHP + Ajax - ulac back to front test simple";
-	$scope.intro = "Coming Soon";
+	$scope.title = "PHP + Ajax - ulac end to end test";
+	$scope.intro = "Coming Soon .... // a preparation for ulac key registration feature ";
 });
-
